ec7e8fefe81d8c60baa2d80ba67bf9aa2e5102bb
Yash Bharadwaj <yash8.mitmpl2022@learner.manipal.edu>
2025-09-28 16:37:09 +0530

    text change

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     <main className="flex items-center max-h-[97vh] overflow-hidden w-full bg-cover bg-center bg-no-repeat flex flex-col">
       <div className="flex flex-col items-center justify-center py-12 text-center z-10 w-full min-w-screen">
         <h1 className="text-7xl font-medium mb-6 mt-6 syne-font">
-          Best Way To Monitor<br /> Your Websites
+          A Decentralized Way <br />To Monitor Your Websites
         </h1>
         <p className="mb-7 text-2xl w-[50%] inter-tight-font">
           Distributed validators interact with our hub to track uptime, latency, and performance so you know issues before your users do.
